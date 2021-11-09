9107f3cca5dffe0d34fc9915baf113a080329282
hadi <hadi.mohammad.saleh1@gmail.com>
2021-11-09 20:28:51 +0300

    testing sending ether

---
+++ learn_src/test/3-partPayment test.js
@@ -0,0 +1,46 @@
+
+const { expect } = require('chai')
+const { ethers, waffle } = require('hardhat')
+const provider = waffle.provider;
+let Loan, loan, signers, payoffAmount, loanDuration, mutex = true
+describe('partPayment function test', () => {
+    beforeEach(async () => {
+
+        if (mutex) {
+            loanDuration = parseInt(Math.floor(Math.random() * 100))
+            payoffAmount = parseInt(Math.floor(Math.random() * 100))
+            mutex = false
+        }
+        await ethers.getSigners().then(res => {
+            signers = res
+        })
+        Loan = await ethers.getContractFactory('Loan')
+        loan = await Loan.deploy(
+            signers[0].address,
+            signers[1].address,
+            payoffAmount,
+            loanDuration)
+    })
+
+    describe('partPayment function works', async () => {
+        it('payoffAmount is updated', async () => {
+            await loan.partPayment(payoffAmount, 0)
+            expect(await loan.payoffAmount()).to.equal(payoffAmount)
+        })
+        it('loanDuration is updated', async () => {
+            await loan.updateLoan(0, loanDuration)
+            expect(await loan.loanDuration()).to.equal(loanDuration)
+        })
+        it("lender's balance is updated", async () => {
+            const overrides = {
+                value: ethers.utils.parseEther("0.0001"), //sending 0.0001 ether
+                from: signers[3].address  
+          }
+            prevBalance = await provider.getBalance(signers[0].address)
+            await loan.connect(signers[3]).partPayment(0,0,overrides)
+            newBalance = await provider.getBalance(signers[0].address)
+            expect(parseInt(newBalance)).to.equal(parseInt(overrides.value ) + parseInt(prevBalance))
+        })
+        //ToDo: updated date?
+    })
+})
